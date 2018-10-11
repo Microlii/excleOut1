2422ffedc4c176b7ca73035433f0bac10d64e62a
Microlii <995405977@qq.com>
2018-10-11 11:11:41 +0800

    主方法

---
+++ src/com/iotek/entity/main.java
@@ -0,0 +1,10 @@
+package com.iotek.entity;
+
+/**
+ * Created by Administrator on 2018/10/11 0011.
+ */
+public class main {
+    public static void main(String[] args) {
+        System.out.println(111);
+    }
+}
